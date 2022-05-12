8055f87b9d719f270e23d362db05bde045569b42
jeremy <jpwijanto@gmail.com>
2022-05-12 13:29:43 +0700

    add max-h

--- components/Difference.js
+++ components/Difference.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Difference() {
   return (
-    <div className="px-4 2xl:px-0 md:px-20 py-[70px] -mt-16 xl:-mt-[90px] bg-[#282626] text-[14px] flex items-center justify-center">
+    <div className="px-4 2xl:px-0 md:px-20 py-[70px] -mt-[80px] xl:-mt-[90px] bg-[#282626] text-[14px] flex items-center justify-center">
       <div className="max-w-[1080px] 2xl:max-w-[1280px] w-full ">
         <h2
           data-aos="fade-down"
--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function Hero() {
   return (
-    <div className="h-screen w-screen overflow-hidden">
+    <div className="h-screen w-screen max-h-screen overflow-hidden">
       <div>
         <Image
           data-aos="slide-down"
